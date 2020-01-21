78990607b080206bb72458ee7e609fb547efa8dd
Christian Valencia <christianvalenciav@gmail.com>
2020-01-21 16:41:23 -0500

    Initial Commit

---
+++ Focus Center/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Focus Center
+//
+//  Created by Christian Valencia on 1/21/20.
+//  Copyright © 2020 Christian Valencia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Focus Center/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Focus Center
+//
+//  Created by Christian Valencia on 1/21/20.
+//  Copyright © 2020 Christian Valencia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
